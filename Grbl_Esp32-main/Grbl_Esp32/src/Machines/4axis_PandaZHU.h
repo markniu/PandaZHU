87a3c2b87518fcef05e5dc31aa5961003e4b0fd4
Mark <niujl123@sina.com>
2021-11-13 16:03:42 +0800

    add PandaZHU for Grbl

--- Grbl_Esp32-main/Grbl_Esp32/src/Machines/4axis_PandaZHU.h
+++ Grbl_Esp32-main/Grbl_Esp32/src/Machines/4axis_PandaZHU.h
@@ -24,7 +24,7 @@
     along with Grbl_ESP32.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-#define MACHINE_NAME            "4 Axis PandaM4 Board"
+#define MACHINE_NAME            "4 Axis PandaZHU Board"
 
 #ifdef N_AXIS
         #undef N_AXIS
@@ -34,7 +34,7 @@
 #endif
 
 #define N_AXIS 4
-#if 1
+
 #define X_STEP_PIN              101
 #define X_DIRECTION_PIN         100
 #define Y_STEP_PIN              103
@@ -43,10 +43,10 @@
 #define Z_DIRECTION_PIN         104
 #define A_STEP_PIN              107
 #define A_DIRECTION_PIN         106
-#define STEPPERS_DISABLE_PIN    115 //X axis
-#define STEPPERS_DISABLE_PIN_Y    114
-#define STEPPERS_DISABLE_PIN_Z    113
-#define STEPPERS_DISABLE_PIN_A    112
+#define STEPPERS_DISABLE_PIN    128 
+#define STEPPERS_DISABLE_PIN_Y    128
+#define STEPPERS_DISABLE_PIN_Z    128
+#define STEPPERS_DISABLE_PIN_A    128
 /*
 #define SPINDLE_TYPE            SpindleType::PWM // only one spindle at a time
 */
@@ -71,46 +71,3 @@
 #define PROBE_PIN               13
 #define COOLANT_MIST_PIN        118
 
-#else
-//#define X_STEP_PIN                          100+16+3
-//#define X_DIR_PIN                           100+16+2
-
-#define X_STEP_PIN              100+16+3
-#define X_DIRECTION_PIN         100+16+2
-#define Y_STEP_PIN            12 // 113
-#define Y_DIRECTION_PIN       14 // 112
-#define Z_STEP_PIN             113// 12
-#define Z_DIRECTION_PIN       112 // 14
-#define A_STEP_PIN              15
-#define A_DIRECTION_PIN         13
-#define STEPPERS_DISABLE_PIN    25 //X axis
-#define STEPPERS_DISABLE_PIN_Y    25
-#define STEPPERS_DISABLE_PIN_Z    25
-#define STEPPERS_DISABLE_PIN_A    25
-/*
-#define SPINDLE_TYPE            SpindleType::PWM // only one spindle at a time
-*/
-
-#define SPINDLE_OUTPUT_PIN      0
-#define SPINDLE_ENABLE_PIN      101
-
-
-#define SPINDLE_TYPE            SpindleType::PWM // only one spindle at a time
-//#define VFD_RS485_TXD_PIN		GPIO_NUM_17
-//#define VFD_RS485_RXD_PIN		GPIO_NUM_4
-//#define VFD_RS485_RTS_PIN		GPIO_NUM_16
-
-#define X_LIMIT_PIN             35
-#define Y_LIMIT_PIN             34
-#define Z_LIMIT_PIN             21
-
-#if (N_AXIS != 3)
-        #define A_LIMIT_PIN     19
-#endif
-
-#define PROBE_PIN               18
-#define COOLANT_MIST_PIN        115
-
-
-
-#endif
